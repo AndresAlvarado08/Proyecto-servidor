3709a0e30fa76bb887919940357ff072403c68a1
Andrés Alvarado <andresalvarado080405@gmail.com>
2024-06-27 12:50:21 -0600

    Test de pull en VS

---
+++ kubejs/startup_scripts/Startup.js
@@ -0,0 +1,14 @@
+// priority: 0
+
+// Visit the wiki for more info - https://kubejs.com/
+
+console.info('Hello, World! (Loaded startup scripts)')
+
+StartupEvents.registry('item', event => {
+    event.create('lapicero').displayName('§4lapicero')
+
+    event.create('papel_con_tinta').displayName('§4Papel con tinta')
+
+    
+
+})
